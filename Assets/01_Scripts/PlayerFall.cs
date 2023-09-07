3a3ac443c9dff2d68befa8db0255bf0d34a683ab
Jihwan Byeon <wkdghk24@gmail.com>
2023-09-07 18:39:12 +0900

    [feat] Play dust effects
    - 플레이어가 절벽에서 떨어지면 먼지 이펙트 발생
    - 먼지 이펙트가 사라지지 않음을 수정 예정

--- Assets/01_Scripts/PlayerFall.cs
+++ Assets/01_Scripts/PlayerFall.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 // 플레이어가 떨어지면
 // 착지하는 곳에 먼지 효과를 넣는다.
+// 요소 : 플레이어 떨어졌는지 여부, 플레이어 위치, 파티클 효과
 public class PlayerFall : MonoBehaviour
 {
+    public GameObject dust;
+    public ParticleSystem ps;
+    bool psStatus = false;
+
+    bool isPlayerFall = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(psStatus == false)
+        {
+            ps.Stop();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        string collisionGo = collision.gameObject.name;
+        if(collisionGo == "FallingGround")
+        {
+            isPlayerFall = true;
+
+            // 플레이어 위치에
+            dust.transform.position = transform.position;
+
+            // 파티클 효과를 실행
+            ps.Play();
+        }
+
+        isPlayerFall = false;
     }
 }
