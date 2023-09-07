0ab09c2300f7a7d0dbac00647aa6e9c46f5add4e
Jihwan Byeon <wkdghk24@gmail.com>
2023-09-07 18:20:22 +0900

    [feat] Connect Player to Oculus
    - 플레이어 이동을 오큘러스와 연결
    - 우측 PrimaryHandTrigger로 이동
    - 테스트용으로 키보드 이동 추가
    - 먼지 파티클 적용 안되면 쉐이더 변경해보기

---
+++ Assets/01_Scripts/PlayerFall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 플레이어가 떨어지면
+// 착지하는 곳에 먼지 효과를 넣는다.
+public class PlayerFall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
