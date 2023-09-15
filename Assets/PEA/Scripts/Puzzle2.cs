163d5e117fe2c91a833c3ab7656aab7fe27dc850
Park-Eunah <90666966+Park-Eunah@users.noreply.github.com>
2023-09-15 12:57:56 +0900

    [feat] Puzzle Reset

--- Assets/PEA/Scripts/Puzzle2.cs
+++ Assets/PEA/Scripts/Puzzle2.cs
@@ -6,27 +6,29 @@ public class Puzzle2 : MonoBehaviour
 {
     public static Puzzle2 instance = null;
 
+    private bool isTrapStart = false;
     private bool isComplete = false;
 
     public Puzzle2_Mural[] murals;
+    public Trap2 trap2;
+
+    public bool IsComplete
+    {
+        get { return isComplete; }
+    }
 
     private void Awake()
     {
         instance = this;
     }
 
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
     public void CheckIsComplete()
     {
+        if (!isTrapStart)
+        {
+            isTrapStart = true;
+            trap2.enabled = true;
+        }
         foreach (Puzzle2_Mural mural in murals)
         {
             if (mural.HasStone)
@@ -38,5 +40,15 @@ public class Puzzle2 : MonoBehaviour
 
         isComplete = true;
         print(isComplete);
+    }
+
+    public void ResetPuzzle()
+    {
+        isTrapStart = false;
+        foreach(Puzzle2_Mural mural in murals)
+        {
+            mural.ResetMural();
+        }
+        trap2.ResetTrap();
     }
 }
---
+++ Assets/PEA/Scripts/Trap2.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap2 : MonoBehaviour
+{
+    private float lerpT = 0f;
+    private readonly float targetPosX = -41f;
+    private Vector3 originPos;
+    private Vector3 targetPos;
+
+    void Start()
+    {
+        originPos = targetPos = transform.position;
+        targetPos.x = targetPosX;
+    }
+
+    void Update()
+    {
+        print(lerpT);
+        lerpT += Time.deltaTime / 10f;
+        transform.position = Vector3.Lerp(originPos, targetPos, lerpT );
+    }
+
+    public void ResetTrap()
+    {
+        lerpT = 0f;
+        transform.position = originPos;
+        enabled = false;
+    }
+}
