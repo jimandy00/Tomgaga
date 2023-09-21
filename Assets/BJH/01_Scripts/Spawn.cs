dbac21d7a4e494e42d70d0cb00364023d853527f
Jihwan Byeon <wkdghk24@gmail.com>
2023-09-21 21:50:15 +0900

    [feat] spawn point
    - spawn point 완성

--- Assets/BJH/01_Scripts/Spawn.cs
+++ Assets/BJH/01_Scripts/Spawn.cs
@@ -1,3 +1,4 @@
+using Oculus.Interaction;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO.IsolatedStorage;
@@ -8,16 +9,14 @@ using UnityEngine.UI;
 // 플레이어가 죽음 상태일 때 스폰 포인트에 플레이어를 위치시킨다.
 public class Spawn : MonoBehaviour
 {
-    public GameObject player;
     bool playerState;
-    Transform savedSpawnPoint;
 
+    Transform savedSpawnPoint;
     
     // Start is called before the first frame update
     void Start()
     {
         playerState = true;
-
     }
 
     // Update is called once per frame
@@ -25,17 +24,20 @@ public class Spawn : MonoBehaviour
     {
         if (playerState == false || Input.GetKeyDown(KeyCode.Q))
         {
-            player.transform.position = savedSpawnPoint.position;
+            transform.position = savedSpawnPoint.position;
         }
 
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        print("스폰 포인트에 닿은 물체 : " + collision.gameObject.name);
-        if(collision.gameObject.name.CompareTo("Player") == 0)
+        print("스폰 포인트에 닿은 물체 : " + other.gameObject.name);
+        string name = other.gameObject.name;
+        
+        if (name.Contains("Spawn"))
         {
-            savedSpawnPoint.position = transform.position;
+            print("플레이어가 트리거에 닿았습니다.");
+            savedSpawnPoint = other.gameObject.transform;
         }
     }
 }
